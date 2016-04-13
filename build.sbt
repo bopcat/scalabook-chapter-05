2029e10d40ee0f6179e9a42674f85c54a620486f
Kirill Prasalov <bopcat@yandex.ru>
2016-04-13 14:56:12 +0200

    1st commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "scalabook-chapter-04"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+    
---
+++ src/main/scala/prasalov/App.scala
@@ -0,0 +1,10 @@
+package prasalov
+
+/**
+ * Created by kirillprasalov on 07.04.16.
+ */
+object App {
+
+  def main(args: Array[String]): Unit = {
+  }
+}
