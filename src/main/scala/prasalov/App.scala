f3171e2e6a6484d30bdf5ab8da252f7d2f8bd5f6
Kirill Prasalov <bopcat@yandex.ru>
2016-04-15 17:33:01 +0200

    2nd commit

--- src/main/scala/prasalov/App.scala
+++ src/main/scala/prasalov/App.scala
@@ -6,5 +6,17 @@ package prasalov
 object App {
 
   def main(args: Array[String]): Unit = {
+    val stream = Stream(1, 2, 3, 4, 5)
+
+    println(stream.toList)
+    println(stream.take(3))
+    println(stream.take(10))
+    println(stream.take(0))
+    println(stream.drop(0).toList)
+    println(stream.drop(3).toList)
+    println(stream.drop(10))
+    println(stream.takeWhile(_ < 3).toList)
+    println(stream.takeWhile(_ < 10).toList)
+    println(stream.takeWhile(_ < 0))
   }
 }
--- src/main/scala/prasalov/Stream.scala
+++ src/main/scala/prasalov/Stream.scala
@@ -1,9 +1,70 @@
 package prasalov
 
+import scala.annotation.tailrec
+
 /**
  * Created by kirillp on 13.04.16.
  */
-sealed trait Stream[+A]
+sealed trait Stream[+A]{
+
+  def toList: List[A] = this match {
+    case Empty => Nil
+    case Cons(h, t) => h() :: t().toList
+  }
+
+  @tailrec final def fold[B](onCons: (() => A, () => Stream[A], B) => B)(res: B, n: Int): B =
+    this match {
+      case Empty => res
+      case Cons(h, t) =>
+        if (n != 0) t().fold(onCons)(onCons(h, t, res), n - 1)
+        else res
+    }
+
+  @tailrec final def foldWhile[B](onCons: (() => A, () => Stream[A], B) => B)(res: B, p: A => Boolean): B =
+    this match {
+      case Empty => res
+      case Cons(h, t) =>
+        if (p(h())) t().foldWhile(onCons)(onCons(h, t, res), p)
+        else res
+    }
+
+  private def extractHead[A](h: () => A, t: () => Stream[A], res: List[A]) = res :+ h()
+  
+  def take(n: Int): List[A] = fold(extractHead)(Nil, n)
+  def drop(n: Int): Stream[A] = fold((h: () => A, t: () => Stream[A], res: Stream[A]) => t())(this, n)
+
+  // straightforward non-tailrec implementation
+  def take1(n: Int): List[A] =
+    if (n == 0) Nil
+    else this match {
+      case Empty => Nil
+      case Cons(h, t) => h() :: t().take(n - 1)
+    }
+
+  // straightforward non-tailrec implementation
+  def drop1(n: Int): Stream[A] =
+    if (n == 0) this
+    else this match {
+      case Empty => this
+      case Cons(h, t) => t().drop(n - 1)
+    }
+
+  def takeWhile(p: A => Boolean): Stream[A] = Stream(foldWhile(extractHead)(Nil, p): _*)
+
+  def foldRight[B](z: => B)(f: (A, =>B) => B): B =
+    this match {
+      case Empty => z
+      case Cons(h, t) => f(h(), t().foldRight(z)(f))
+    }
+
+  class CountdownFunc(initial: Int) {
+    var count = initial
+    def apply = {
+      count = count - 1
+      count + 1 == 0
+    }
+  }
+}
 
 case object Empty extends Stream[Nothing]
 
